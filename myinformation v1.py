18fda453b484d6601884c9d0e3e205855c1857a2
LocusExile <146225333+LocusExile@users.noreply.github.com>
2024-01-18 12:57:51 -0500

    My Information Part I
    My Information Part I

---
+++ myinformation v1.py
@@ -0,0 +1,8 @@
+#CS175L
+#Gavin Kinsella
+#myInfo
+
+print("My name is: Gavin Kinsella")
+print("My address is 143 Chestnut Ave Alantic Highlands NJ 07716")
+print("my phone number is: 732-947-0013")
+      
