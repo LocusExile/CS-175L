e962f55d53206c73381a25e5e1c0a3fc36454f6f
LocusExile <146225333+LocusExile@users.noreply.github.com>
2024-02-05 11:58:12 -0500

    Restaurant Program
    Restaurant Program

---
+++ res.py
@@ -0,0 +1,38 @@
+#CS175L
+#Gavin Kinsella
+#restaurant
+
+vegetarian = input("Is anyone in your party vegetarian? (yes/no): ")
+vegan = input("Is anyone in your party vegan? (yes/no): ")
+gluten_free = input("Is anyone in your party gluten-free? (yes/no): ")
+
+print("\nHere are the suitable restaurants for your group:")
+if vegetarian == 'yes' and vegan == 'no' and gluten_free == 'no':
+    print("- Mama’s Fine Italian")
+    print("- Main Street Pizza Company")
+    print("- Corner Café")
+    print("- The Chef’s Kitchen")
+elif vegetarian == 'yes' and vegan == 'no' and gluten_free == 'yes':
+    print("- Corner Café")
+    print("- Mama’s Fine Italian")
+    print("- The Chef’s Kitchen")
+elif vegetarian == "no" and vegan == 'no' and gluten_free == "yes":
+    print("- Corner Café")
+    print("- The Chef’s Kitchen")
+    print("- Main Street Pizza Company")
+elif vegetarian == "yes" and vegan == 'no' and gluten_free == "yes":
+    print("- Corner Café")
+    print("- The Chef’s Kitchen")
+    print("- Main Street Pizza Company")
+elif vegetarian == "no" and vegan == 'no' and gluten_free == 'no':
+    print("- Joe's Gorumet Burgers")
+    print("- Main Street Pizza Company")
+    print("- Corner Café")
+    print("- Mama’s Fine Italian")
+    print("- The Chef’s Kitchen")
+else:
+    print("- Corner Café")
+    print("- The Chef’s Kitchen")
+        
+
+
