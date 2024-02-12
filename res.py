e48ca9c3a02de20abfb22db71684e1954ebb720c
LocusExile <146225333+LocusExile@users.noreply.github.com>
2024-02-12 12:11:34 -0500

    Add files via upload

--- res.py
+++ res.py
@@ -1,38 +1,42 @@
 #CS175L
 #Gavin Kinsella
 #restaurant
+while True: 
+        vegetarian = input("Is anyone in your party vegetarian? (yes/no): ")
+        vegan = input("Is anyone in your party vegan? (yes/no): ")
+        gluten_free = input("Is anyone in your party gluten-free? (yes/no): ")
+         
+        print("\nHere are the suitable restaurants for your group:")
+        if vegetarian == 'yes' and vegan == 'no' and gluten_free == 'no':
+            print("- Mama’s Fine Italian")
+            print("- Main Street Pizza Company")
+            print("- Corner Café")
+            print("- The Chef’s Kitchen")
+        elif vegetarian == 'yes' and vegan == 'no' and gluten_free == 'yes':
+            print("- Corner Café")
+            print("- Mama’s Fine Italian")
+            print("- The Chef’s Kitchen")
+        elif vegetarian == "no" and vegan == 'no' and gluten_free == "yes":
+            print("- Corner Café")
+            print("- The Chef’s Kitchen")
+            print("- Main Street Pizza Company")
+        elif vegetarian == "yes" and vegan == 'no' and gluten_free == "yes":
+            print("- Corner Café")
+            print("- The Chef’s Kitchen")
+            print("- Main Street Pizza Company")
+        elif vegetarian == "no" and vegan == 'no' and gluten_free == 'no':
+            print("- Joe's Gorumet Burgers")
+            print("- Main Street Pizza Company")
+            print("- Corner Café")
+            print("- Mama’s Fine Italian")
+            print("- The Chef’s Kitchen")
+        else:
+            print("- Corner Café")
+            print("- The Chef’s Kitchen")
 
-vegetarian = input("Is anyone in your party vegetarian? (yes/no): ")
-vegan = input("Is anyone in your party vegan? (yes/no): ")
-gluten_free = input("Is anyone in your party gluten-free? (yes/no): ")
-
-print("\nHere are the suitable restaurants for your group:")
-if vegetarian == 'yes' and vegan == 'no' and gluten_free == 'no':
-    print("- Mama’s Fine Italian")
-    print("- Main Street Pizza Company")
-    print("- Corner Café")
-    print("- The Chef’s Kitchen")
-elif vegetarian == 'yes' and vegan == 'no' and gluten_free == 'yes':
-    print("- Corner Café")
-    print("- Mama’s Fine Italian")
-    print("- The Chef’s Kitchen")
-elif vegetarian == "no" and vegan == 'no' and gluten_free == "yes":
-    print("- Corner Café")
-    print("- The Chef’s Kitchen")
-    print("- Main Street Pizza Company")
-elif vegetarian == "yes" and vegan == 'no' and gluten_free == "yes":
-    print("- Corner Café")
-    print("- The Chef’s Kitchen")
-    print("- Main Street Pizza Company")
-elif vegetarian == "no" and vegan == 'no' and gluten_free == 'no':
-    print("- Joe's Gorumet Burgers")
-    print("- Main Street Pizza Company")
-    print("- Corner Café")
-    print("- Mama’s Fine Italian")
-    print("- The Chef’s Kitchen")
-else:
-    print("- Corner Café")
-    print("- The Chef’s Kitchen")
-        
-
-
+        another_search = input('Do you want to do antoher search (yes/no) ')
+        if another_search.lower() != 'yes':
+                print("This is the end of the program")
+                break
+                               
+               
