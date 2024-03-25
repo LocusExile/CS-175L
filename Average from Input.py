ea37c0966efe8b43d59ee0a5bf43aa76ac01e87e
LocusExile <146225333+LocusExile@users.noreply.github.com>
2024-03-25 11:37:45 -0400

    Add files via upload

--- Average from Input.py
+++ Average from Input.py
@@ -1,15 +1,27 @@
-def calculate_avg(file_name):
-    with open (file_name, 'r') as file:
-        total = 0
-        count = 0
-        for line in file:
-            number = int(line.strip())
-            count += 1
-            total += number
-            print(f"I read in {count} number(s) Current number is: {number:8.2f} Total is: {total:8.2f}")
+#Gavin Kinsella
+#CS-175L
+#Calculate Average from Input
 
-    average = total / count if count > 0 else 0
-    print(f"Average: {average:.1f}")
+def calculate_avg(file_name):
+    try:
+        with open (file_name, 'r') as file:
+            total = 0
+            count = 0
+            for line in file:
+                try:
+                    number = int(line.strip())
+                except ValueError:
+                    print(f"Error: Bad Input {line.strip()} skipping")
+                    continue
+                count += 1
+                total += number
+                print(f"I read in {count} number(s) Current number is: {number:8.2f} Total is: {total:8.2f}")
+
+        average = total / count if count > 0 else 0
+        print(f"Average: {average:.1f}")
+
+    except IOError:
+        print(f"SystemExit: File not found: '{file_name}'-exiting")
 
 def main():
     file_name = 'numbers.txt'
